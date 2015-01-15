88b062ab6e360967ea7a4d149c8c663144ca7a0f
Mike Jones <mike@westerncapelabs.com>
2015-01-15 20:58:51 +0200

    Another travis style test

--- django_usaid_clinicfinder/runtests.sh
+++ django_usaid_clinicfinder/runtests.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-createuser --createdb -R -S postgis_test
-psql -c 'ALTER ROLE postgis_test SUPERUSER;'
+
 export DJANGO_SETTINGS_MODULE="django_usaid_clinicfinder.testsettings"
 ./manage.py test "$@"
-dropuser postgis_test
