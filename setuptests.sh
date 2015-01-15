dc2bd1979888653c89e6b7e2536c1aa5dc26c854
Mike Jones <mike@westerncapelabs.com>
2015-01-15 20:47:44 +0200

    Add test_ prefix

--- setuptests.sh
+++ setuptests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-psql -c 'create database django_usaid_clinicfinder;' -U postgres
-psql -c 'CREATE EXTENSION hstore;' -U postgres -d django_usaid_clinicfinder
-psql -c 'CREATE EXTENSION postgis;' -U postgres -d django_usaid_clinicfinder
-psql -c 'CREATE EXTENSION postgis_topology;' -U postgres -d django_usaid_clinicfinder
+psql -c 'create database test_django_usaid_clinicfinder;' -U postgres
+psql -c 'CREATE EXTENSION hstore;' -U postgres -d test_django_usaid_clinicfinder
+psql -c 'CREATE EXTENSION postgis;' -U postgres -d test_django_usaid_clinicfinder
+psql -c 'CREATE EXTENSION postgis_topology;' -U postgres -d test_django_usaid_clinicfinder
 
 echo "DATABASES = {'default': {'ENGINE': 'django.contrib.gis.db.backends.postgis','NAME': 'django_usaid_clinicfinder','USER': 'postgres'}}" > django_usaid_clinicfinder/django_usaid_clinicfinder/local_settings.py
