09923fed3ee839cfc3d7d362912e76b4c1ffcf60
Mike Jones <mike@westerncapelabs.com>
2015-01-15 20:38:05 +0200

    Syntax fix

--- setuptests.sh
+++ setuptests.sh
@@ -3,4 +3,4 @@ psql -c 'create database django_usaid_clinicfinder;' -U postgres
 psql -c 'CREATE EXTENSION postgis;' -U postgres -d django_usaid_clinicfinder
 psql -c 'CREATE EXTENSION postgis_topology;' -U postgres -d django_usaid_clinicfinder
 
-echo "DATABASES = 'default': {'ENGINE': 'django.contrib.gis.db.backends.postgis','NAME': 'django_usaid_clinicfinder','USER': 'postgres',}}" > django_usaid_clinicfinder/django_usaid_clinicfinder/local_settings.py
+echo "DATABASES = {'default': {'ENGINE': 'django.contrib.gis.db.backends.postgis','NAME': 'django_usaid_clinicfinder','USER': 'postgres'}}" > django_usaid_clinicfinder/django_usaid_clinicfinder/local_settings.py
