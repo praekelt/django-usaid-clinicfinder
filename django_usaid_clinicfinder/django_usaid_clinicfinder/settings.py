d05cc2d57a2c052a921ac05ba3e094befda55e2b
Mike Jones <mike@westerncapelabs.com>
2015-01-16 09:52:15 +0200

    Blank out settings

--- django_usaid_clinicfinder/django_usaid_clinicfinder/settings.py
+++ django_usaid_clinicfinder/django_usaid_clinicfinder/settings.py
@@ -19,14 +19,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'REPLACEMEPLSKTNKSBAI'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = None
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = None
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'mmc.qa.praekelt.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
