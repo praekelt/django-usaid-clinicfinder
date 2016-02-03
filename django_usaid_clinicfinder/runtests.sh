3db3d1369ec78a3bc024d49ded956370f1aee52d
TrevorOctober <trevor@projectcodex.co>
2016-02-03 11:11:23 +0200

    fix runtests.sh file

--- django_usaid_clinicfinder/runtests.sh
+++ django_usaid_clinicfinder/runtests.sh
@@ -2,4 +2,4 @@
 
 export DJANGO_SETTINGS_MODULE="django_usaid_clinicfinder.testsettings"
 cd django_usaid_clinicfinder
-./manage.py test clinicfinder/tests.py:: --settings="django_usaid_clinicfinder.testsettings"
+./manage.py test "$@"
